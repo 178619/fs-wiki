44a9d5eecc25273734ccc3c30ef9f6fbdc157829
Fryze <fryze@floatick.com>
2025-07-28 07:17:37 +0900

    wiki

--- wiki/custom.js
+++ wiki/custom.js
@@ -10,7 +10,8 @@ You can check [[Fortune Summoners]] page or search articles at the top.
 `
 
 Wiklo.moduleHandlers = {...Wiklo.moduleHandlers, ...{
-    'flag unicode': (args, kwargs) => (args[0]||'').toUpperCase().split('').map(v=>String.fromCodePoint(v.charCodeAt()+0x1F1A5)).join('')
+    'flag unicode': (args, kwargs) => (args[0]||'').toUpperCase().split('').map(v=>String.fromCodePoint(v.charCodeAt()+0x1F1A5)).join(''),
+    'fs key': (args, kwargs) => args[0] ? (args[0].split(/(\+|\-)/).map(v=>'{{nestimage|_fskey_'+v+'.png|class=fskey}}')).join('') : ''
 }}
 
 })()
