f0c83f0e4b0d7730b1872219f3b5301cbb0eacd8
Fryze <fryze@floatick.com>
2025-07-28 16:34:08 +0900

    wiki

--- wiki/custom.js
+++ wiki/custom.js
@@ -4,9 +4,20 @@ Wiklo.title = 'Fortune Summoners Collection'
 
 Wiklo.home = `
 
-Welcome to the Fortune Summoners Collection.
+Welcome to the [[Fortune Summoners]] Collection.
 
-You can check [[Fortune Summoners]] page or search articles at the top.
+Although this page is named Fortune Summoners Collection, it aims to document all products from [[Lizsoft]] and their affiliates.
+
+You can search articles at the top to browse pages.
+
+Note:
+This webpage uses Hepburn style without diacritics to transliterate Japanese names.
+It also respects the native form of the personal names, causing family names to be placed ''before'' given names.
+However, if the right owner consistenly uses their own version for their ''romaji'' name or openly endorses it, it will be respected and take priority.
+
+<details>
+    <summary>Open Recent Changes</summary>{{articlelist|sortedby=lastModification|reversed=1}}
+</details>
 `
 
 Wiklo.moduleHandlers = {...Wiklo.moduleHandlers, ...{
