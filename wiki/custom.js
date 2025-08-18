b74d03a9e66b0d890388652fdc9fcb2933c7e397
Fryze <fryze@floatick.com>
2025-08-19 02:48:19 +0900

    wiki

--- wiki/custom.js
+++ wiki/custom.js
@@ -13,7 +13,7 @@ You can search articles at the top to browse pages.
 Note:
 This webpage uses Hepburn style without diacritics to transliterate Japanese names.
 It also respects the native form of the personal names, causing family names to be placed ''before'' given names.
-However, if the right owner consistenly uses their own version for their ''romaji'' name or openly endorses it, it will be respected and take priority.
+However, if the right owner consistently uses their own version for their ''romaji'' name or openly endorses it, it will be respected and take priority.
 
 <details>
     <summary>Open Recent Changes</summary>{{articlelist|sortedby=lastModification|reversed=1}}
