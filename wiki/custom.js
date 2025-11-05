e96000466e0af07eb9e5711f6ec036d3386fd60b
Fryze <fryze@floatick.com>
2025-11-05 14:51:36 +0900

    wiki

--- wiki/custom.js
+++ wiki/custom.js
@@ -20,9 +20,29 @@ However, if the right owner consistently uses their own version for their ''roma
 </details>
 `
 
+fsKeySortTable = {
+    'right': '6',
+    'up': '7',
+    'down': '8',
+    'left': '9',
+    'z': '1',
+    'x': '2',
+    'c': '3',
+    '+': '+',
+    '-': '-',
+    'f1': 'F1',
+    'f2': 'F2',
+    'f3': 'F3',
+    'f4': 'F4',
+    'f5': 'F5',
+    'enter': 'F6'
+}
+
 Wiklo.moduleHandlers = {...Wiklo.moduleHandlers, ...{
     'flag unicode': (args, kwargs) => (args[0]||'').toUpperCase().split('').map(v=>String.fromCodePoint(v.charCodeAt()+0x1F1A5)).join(''),
-    'fs key': (args, kwargs) => args[0] ? (args[0].split(/(\+|\-)/).map(v=>'{{nestimage|_fskey_'+v+'.png|class=fskey}}')).join('') : ''
+    'fs key': (args, kwargs) => args[0] ? ('<span class="fskey-array" data-sort-value="!FSKEY_'+args[0].split(/(\+|\-)/).map(v=>fsKeySortTable[v]||v).join('')+'">' + args[0].split(/(\+|\-)/).map(v=>'{{nestimage|_fskey_'+v+'.png|class=fskey|alt='+v+'}}').join('') + '</span>') : '',
+    'fs level': (args, kwargs) => `<span data-sort-value="!FSLEVEL_${args[0].padStart(2,'0')}">${args[0] != '-1' ? ((args[1] || 'Level') + ' ' + args[0]) : 'None'}</span>`,
+    'fs mp': (args, kwargs) => `<span data-sort-value="!FSMP_${args[0].padStart(2,'0')}">${args[0] != '-1' ? (args[0] + ' ' + (args[1] || 'MP')) : 'None'}</span>`
 }}
 
 })()
